1b77c12b554127450bad2780c5f4e8bd51cab7ee
Maxwell Chow <maxchowla@gmail.com>
2015-12-09 11:54:55 -0800

    User method

--- index.js
+++ index.js
@@ -46,7 +46,7 @@ function queryAPI(method, v, key, format, adds, callback) {
 function getMarketPrices(key, appid, callback) {
   queryAPI('IGetMarketPrices', 'v1', key, 'json', '&appid=' + appid, function(data) {
     if (data.response.success === 0) {
-      callback(new Error(data.response.message))
+      callback(new Error(data.response.message));
     } else {
       callback(undefined, data);
     }
@@ -72,6 +72,29 @@ function getBPPrices(key, appid, callback) {
       callback(new Error(data.response.message));
     } else {
       callback(undefined, data);
+    }
+  });
+}
+
+/*
+    backpacktf.getUser()
+    Retrieves backpack.tf price data for specified appid.
+    Parameters:
+      key: backpack.tf api key
+      steamids: list of users to retrieve data on, delimited by commas
+      callback: called when backpacks are retrieved
+        Callback Arguments:
+          err: an Error object with the reason for failure,
+            undefined on success
+          data: an Object containing the response
+*/
+
+function getUser(key, steamids, callback) {
+  queryAPI('IGetUsers', 'v3', key, 'json', '&steamids=' + steamids, function(data) {
+    if (data.response.success === 0) {
+      callback(new Error(data.response.message));
+    } else {
+      callback(undefined, data)
     }
   });
 }
