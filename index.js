5a3d1c8d377b90e2ccb78eef8b102ae3d5451989
Maxwell Chow <maxchowla@gmail.com>
2016-01-06 11:12:22 -0800

    DataCache full-ish implementation

--- index.js
+++ index.js
@@ -7,16 +7,39 @@ var Poller = poller.Poller;
 
 function DataCache(key) {
   this.cachedData = [{
-    prices: "marketPrices440",
+    query: "marketPrices440",
     poller: new Poller(function(callback) {
       methods.getMarketPrices(key, 440, function(err, data) {
         if (!err) {
           callback(data);
         }
-      })
+      });
+    })
+  },{
+    query: "marketPrices730",
+    poller: new Poller(function(callback) {
+      methods.getMarketPrices(key, 730, function(err, data) {
+        if (!err) {
+          callback(data);
+        }
+      });
     })
   }];
 }
+
+DataCache.prototype.startPollingAll = function() {
+  for (var i = 0; i < this.cachedData.length; i++) {
+    this.cachedData[i].poller.startAsyncStore(10 * 60 * 1000);
+  }
+};
+
+DataCache.prototype.getData = function() {
+  var ret = new Array();
+  for (var i = 0; i < this.cachedData.length; i++) {
+    ret.push(this.cachedData[i].poller.retrieveStorage());
+  }
+  return ret;
+};
 
 module.exports = {
   AppIDs: IDs.AppIDs,
@@ -24,5 +47,6 @@ module.exports = {
   UnusualIDs: IDs.UnusualEffects,
   getMarketPrices: methods.getMarketPrices,
   getBPPrices: methods.getBPPrices,
-  poller: poller.Poller
+  Poller,
+  DataCache
 };
